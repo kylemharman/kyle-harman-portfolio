be703492efa7d208383e0d92a1ec9dfba972062a
“kylemharman” <kylemartinharman@gmail.com>
2020-01-01 22:41:33 +1100

    updated nav

--- src/components/NavigationOverlay.js
+++ src/components/NavigationOverlay.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from '@reach/router';
 // styles
 import {NavigationOverlayNav , StyledNavLink} from '../styles/NavigationOverlayStyled';
 
@@ -6,12 +7,23 @@ const NavigationOverlay = (props) => {
 
     return (
         <NavigationOverlayNav active={props.active} onClick={props.clicked}>
-            <StyledNavLink to="/" background="https://images.unsplash.com/photo-1507561152-ed3719ac59f1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=882&q=80">Home.</StyledNavLink>
+            <StyledNavLink to="/" background="https://images.unsplash.com/photo-1568160277762-0224a391b5a5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=939&q=80">Home.</StyledNavLink>
             <StyledNavLink to="/about" background="https://images.unsplash.com/photo-1554188248-986adbb73be4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80">About.</StyledNavLink>
             <StyledNavLink to="/projects" background="https://images.unsplash.com/photo-1565855991995-7a5ceb19ee95?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80">Projects.</StyledNavLink>
-            <StyledNavLink to="/contact" background="https://images.unsplash.com/photo-1568160277762-0224a391b5a5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=939&q=80">Contact.</StyledNavLink>
-        </NavigationOverlayNav> 
+            <StyledNavLink to="/contact" background="https://images.unsplash.com/photo-1564089957880-517edea1afc5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80">Contact.</StyledNavLink>
+            <div>
+                <p>Connect </p>
+                <ul>
+                    <li><Link to="#"><i className="fab fa-github fa-lg"></i></Link></li>
+                    <li><Link to="#"><i className="fab fa-linkedin-in fa-lg"></i></Link></li>
+                </ul>
+            </div>
+            
+        </NavigationOverlayNav>
+         
     )
 }
 
 export default NavigationOverlay;
+
+
--- src/styles/NavigationOverlayStyled.js
+++ src/styles/NavigationOverlayStyled.js
@@ -6,14 +6,40 @@ export const NavigationOverlayNav = styled.nav`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  background-color: whitesmoke;
+  background-color: white;
   height: 100vh;
   width: 100%;
   position: absolute;
   top: 0;
   left: 0;
-  transition: all 0.3s ease-out;
-  transform: ${props => (props.active ? "translateY(0%)" : "translateY(-100%)")};
+  transition: all 0.5s ease-in-out;
+  transform: ${props => (props.active ? "translateX(0%)" : "translateX(+100%)")};
+
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    top: 45vh;
+    right: 10px;
+    
+  }
+
+  p {
+    transform: rotate(90deg);   
+  }
+
+  ul {
+    margin: 10px;
+    padding: 0;
+    text-decoration: none;
+    list-style: none;
+
+    & li {
+      margin: 10px;
+    }
+  }
 `
 
 export const StyledNavLink = styled(Link) `
@@ -23,7 +49,7 @@ export const StyledNavLink = styled(Link) `
     margin: 0.5rem;
     -webkit-text-stroke-width: 1px;
     -webkit-text-stroke-color: black;
-    color: whitesmoke;
+    color: white;
     transition: all 0.3s ease-in-out;
     
     &:hover {
